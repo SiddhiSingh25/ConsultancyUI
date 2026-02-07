c5bb9928a6f990d18d075328cc27a5010658a35d
Siddhi Singh <141171905+SiddhiSingh25@users.noreply.github.com>
2026-02-07 13:02:58 +0530

    Change section background color to gray and update padding

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -3,7 +3,7 @@ import Heading from './common/Heading'
 
 const WhyChooseUs = () => {
   return (
-    <section className="bg-white py-12 px-4">
+    <section className="bg-gray-50  py-12 px-4 md:px-16 lg:px-24">
   <div className="max-w-7xl mx-auto">
 
 
